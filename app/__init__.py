ac7514f55c9ef1f9b2902777ebcbda30b37bf344
David Wu <da853127@gmail.com>
2022-12-17 19:44:34 +0800

    debug test

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,24 @@
+# -*- coding: UTF-8 -*-
+
 from flask import Flask,jsonify,request
 from flask_cors import CORS
 import numpy as np 
 from app.calculate import plus
 
-
-
 app = Flask(__name__)
 CORS(app)
-
-
-
+@app.route('/test',methods = ['GET'])
+def test1():
+    values = 2
+    return jsonify({'return':'ok'})
 
 @app.route('/predict', methods = ['GET','POST'])
 def test():
-    values = request.get_json()
-    x1 = values['x1']
-    x2 = values['x2']
-    data = np.array([x1,x2])
-    ans = plus(data)
+    # values = request.get_json()
+    # x1 = values['x1']
+    # x2 = values['x2']
+    # data = np.array([x1,x2])
+    ans = 2
     
-    return jsonify({'return':str(ans)})
+    return jsonify({'return':'ok'})
 
--- app/calculate.py
+++ app/calculate.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 def plus(input):
     ans = input[0]+input[1]
     return ans
