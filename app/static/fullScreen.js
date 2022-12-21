2c477fd9349e48a5e82fc6adc0bf2c9ca68a0e30
David Wu <da853127@gmail.com>
2022-12-21 16:23:25 +0800

    debug safari fullscreen

--- app/static/fullScreen.js
+++ app/static/fullScreen.js
@@ -4,28 +4,28 @@ let btn = document.getElementById('btn-fullScreen')
 let isFullScreen = false
 btn.addEventListener('click',()=>{
     if(isFullScreen == false){
-        myDocument.requestFullscreen()
-        // if(myDocument.requestFullscreen){
-        //     myDocument.requestFullscreen()
-        // }else if(myDocument.msRequestFullscreen){
-        //     myDocument.msRequestFullscreen()
-        // }else if(myDocument.mozRequestFullscreen){
-        //     myDocument.mozRequestFullscreen()
-        // }else if(myDocument.webkitRequestFullscreen){
-        //     myDocument.webkitRequestFullscreen()
-        // }
+        // myDocument.requestFullscreen()
+        if(myDocument.requestFullscreen){
+            myDocument.requestFullscreen()
+        }else if(myDocument.msRequestFullscreen){
+            myDocument.msRequestFullscreen()
+        }else if(myDocument.mozRequestFullscreen){
+            myDocument.mozRequestFullscreen()
+        }else if(myDocument.webkitRequestFullscreen){
+            myDocument.webkitRequestFullscreen()
+        }
         isFullScreen = true
     }else{
-        document.exitFullscreen()
-        // if(document.exitFullscreen){
         // document.exitFullscreen()
-        // }else if(document.msexitFullscreen){
-        //     document.msexitFullscreen()
-        // }else if(document.mozexitFullscreen){
-        //     document.mozexitFullscreen()
-        // }else if(document.webkitexitFullscreen){
-        //     document.webkitexitFullscreen()
-        // }
+        if(document.exitFullscreen){
+        document.exitFullscreen()
+        }else if(document.msexitFullscreen){
+            document.msexitFullscreen()
+        }else if(document.mozexitFullscreen){
+            document.mozexitFullscreen()
+        }else if(document.webkitExitFullscreen){
+            document.webkitExitFullscreen()
+        }
         isFullScreen = false
     }    
 })
